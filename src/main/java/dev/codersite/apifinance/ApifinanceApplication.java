ef148722b87d48b96666b1c0b0c107d07c275185
Moisés Gamio Vergara <MGV@selly.biz>
2025-06-12 11:04:18 +0200

    initial commit

---
+++ src/main/java/dev/codersite/apifinance/ApifinanceApplication.java
@@ -0,0 +1,14 @@
+package dev.codersite.apifinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifinanceApplication.class, args);
+		System.out.println("Hola Berlin!");
+	}
+
+}
---
+++ src/test/java/dev/codersite/apifinance/ApifinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codersite.apifinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
